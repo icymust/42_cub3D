b38163c7377a83c7a76e5a6199bcf73666cc0afd
Martin Mustonen <mmustone@lab2r2s13.42abudhabi.ae>
2026-06-17 14:20:55 +0400

    draw floor and ceiling

--- src/render.c
+++ src/render.c
@@ -6,41 +6,45 @@
 /*   By: mmustone <mmustone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 15:14:39 by mmustone          #+#    #+#             */
-/*   Updated: 2026/06/17 12:49:51 by mmustone         ###   ########.fr       */
+/*   Updated: 2026/06/17 14:17:14 by mmustone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-static void put_pixel(t_img *img, int x, int y, int color)
+static void	put_pixel(t_img *img, int x, int y, int color)
 {
-    char *dst;
-    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-    *(unsigned int *)dst = color;
+	char	*dst;
+
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
-static void fill_screen(t_game *game, int color)
+static void	draw_floor_ceiling(t_game *game)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    y = 0;
-    while(y < game->vars.win_height)
-    {
-        x = 0;
-        while(x < game->vars.win_width)
-        {
-            put_pixel(&game->screen, x, y, color);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < game->vars.win_height)
+	{
+		x = 0;
+		while (x < game->vars.win_width)
+		{
+			if (y < game->vars.win_height / 2)
+				put_pixel(&game->screen, x, y, game->config.ceiling_color);
+			else
+				put_pixel(&game->screen, x, y, game->config.floor_color);
+			x++;
+		}
+		y++;
+	}
 }
 
 int	render_frame(t_game *game)
 {
-	fill_screen(game, game->config.floor_color);
-	mlx_put_image_to_window(game->vars.mlx, game->vars.win,
-		game->screen.img, 0, 0);
+	draw_floor_ceiling(game);
+	mlx_put_image_to_window(game->vars.mlx, game->vars.win, game->screen.img, 0,
+		0);
 	return (0);
 }
